b69693c3e2b6066273efa9f84e855c254639f133
kiran <askiran123@gmail.com>
2015-12-29 23:53:53 +0530

    deleted files

--- Sangam/admin/include/nav.php
+++
@@ -1,7 +0,0 @@
-<div class="main-nav-wrapper">
-       <nav class="container">
-           <ul class="main-nav clearfix">
-               <li class="active"><a href="tariffList.php">Tariff</a></li>
-           </ul>
-       </nav>
-    </div><!--/Main Nav Ends-->
